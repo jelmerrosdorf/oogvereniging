a39196f978749a774bd89eebe4902b47324f27fc
Jelmer Rosdorf <jelmerr99@gmail.com>
2023-06-08 12:19:13 +0200

    Add migration for events table

---
+++ database/migrations/2023_06_01_155807_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 255);
+            $table->string('datetime_start', 50);
+            $table->string('datetime_end', 50);
+            $table->string('location', 255);
+            $table->string('price', 10);
+            $table->string('price_member', 10);
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
