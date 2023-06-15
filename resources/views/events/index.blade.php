3095fde939d6d9e7e6b53a1cf0ccc889a26c48b1
Jelmer Rosdorf <jelmerr99@gmail.com>
2023-06-15 23:30:48 +0200

    Change event display to only show public events on index page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -39,6 +39,7 @@
                     tracking-wide rounded shadow hover:border hover:border-oogvereniging-blue
                     focus:border focus:border-oogvereniging-blue">
                         <option value="alles">Alles</option>
+                        <option value="Landelijk">Landelijk</option>
                         <option value="zuid-holland">Zuid-Holland</option>
                         <option value="noord-holland">Noord-Holland</option>
                         <option value="noord-brabant">Noord-Brabant</option>
@@ -57,6 +58,7 @@
                         <option value="digiwijs">Digiwijs</option>
                         <option value="glaucoom">Glaucoom</option>
                         <option value="oor&oog">Oor & oog</option>
+                        <option value="sportief">Sportief</option>
                     </select>
                 </form>
             </div>
@@ -65,32 +67,35 @@
                 Activiteiten
             </h2>
 
-            <div class="flex justify-between py-6">
+            <div class="flex flex-wrap justify-between py-6">
                 @foreach ($events as $event)
-                    <div onclick="location.href='{{ route('events.show',$event->id) }}';"
-                         class="border-2 border-oogvereniging-blue rounded bg-oogvereniging-white
-                          text-oogvereniging-blue font-semibold p-5 shadow-xl w-4,5/10 flex
-                          flex-col justify-between gap-8 focus:border-oogvereniging-red
-                          hover:border-oogvereniging-red focus:cursor-pointer hover:cursor-pointer">
-                        <h3 class="text-2xl mb-4 underline underline-offset-4">{{ $event->title
-                        }}</h3>
-                        <div class="flex justify-between items-baseline">
-                            <p class="text-lg mb-4 tracking-wide">{{
-                            $event->datetime_start }}</p>
-                            <div class="flex gap-4">
-                                @if($event->tag_province != '')
-                                    <p class="tracking-wide border rounded
-                                    border-oogvereniging-black bg-oogvereniging-purple
-                                    text-oogvereniging-white p-2">{{ $event->tag_province }}</p>
-                                @endif
-                                @if($event->tag_subject != '')
-                                    <p class="tracking-wide border rounded
-                                    border-oogvereniging-black bg-oogvereniging-purple
-                                    text-oogvereniging-white p-2">{{ $event->tag_subject }}</p>
-                                @endif
+                    @if($event->public == 1)
+                        <div onclick="location.href='{{ route('events.show',$event->id) }}';"
+                             class="border-2 border-oogvereniging-blue rounded bg-oogvereniging-white
+                              text-oogvereniging-blue font-semibold p-5 shadow-xl w-4,5/10 flex
+                              flex-col justify-between gap-8 focus:border-oogvereniging-red
+                              hover:border-oogvereniging-red focus:cursor-pointer
+                              hover:cursor-pointer mb-12">
+                            <h3 class="text-2xl mb-4 underline underline-offset-4">
+                            {{ $event->title }}</h3>
+                            <div class="flex justify-between items-baseline">
+                                <p class="text-lg mb-4 tracking-wide">{{
+                                $event->datetime_start }}</p>
+                                <div class="flex gap-4">
+                                    @if($event->tag_province != '')
+                                        <p class="tracking-wide border rounded
+                                        border-oogvereniging-black bg-oogvereniging-purple
+                                        text-oogvereniging-white p-2">{{ $event->tag_province }}</p>
+                                    @endif
+                                    @if($event->tag_subject != '')
+                                        <p class="tracking-wide border rounded
+                                        border-oogvereniging-black bg-oogvereniging-purple
+                                        text-oogvereniging-white p-2">{{ $event->tag_subject }}</p>
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
 
