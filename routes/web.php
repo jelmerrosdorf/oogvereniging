65067dabdd81a9a0e0230c8ce8dc45ed52145273
Jelmer Rosdorf <jelmerr99@gmail.com>
2024-05-31 03:42:37 +0200

    Simplify routing loop for MVP purposes - remove dashboard and welcome

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/events');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
