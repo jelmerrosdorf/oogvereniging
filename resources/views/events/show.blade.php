03895c0633bb0996c9bcb6789d1353a45c3bed9a
Jelmer Rosdorf <jelmerr99@gmail.com>
2024-05-31 03:46:56 +0200

    Change signup button to only show if event is published

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -51,20 +51,22 @@
         <div class="max-w-6xl mx-auto my-6">
             <p class="text-lg text-oogvereniging-blue">Aanmelden leden</p>
             <p class="text-xl font-semibold text-oogvereniging-blue tracking-wide mt-1"></p>
-            @if ($isSignedUp)
-                <form action="{{ route('event.signout', $event->id) }}" method="POST">
-                    @csrf
-                    <button class="text-xl font-semibold tracking-wide text-oogvereniging-white
-                bg-oogvereniging-red px-5 py-3 mt-1 rounded-lg border border-oogvereniging-black
-                shadow">Afmelden</button>
-                </form>
-            @else
-                <form action="{{ route('event.signup', $event->id) }}" method="POST">
-                    @csrf
-                    <button class="text-xl font-semibold tracking-wide text-oogvereniging-white
-                bg-oogvereniging-red px-5 py-3 mt-1 rounded-lg border border-oogvereniging-black
-                shadow">Direct aanmelden</button>
-                </form>
+            @if ($event->public == 1)
+                @if ($isSignedUp)
+                    <form action="{{ route('event.signout', $event->id) }}" method="POST">
+                        @csrf
+                        <button class="text-xl font-semibold tracking-wide text-oogvereniging-white
+                    bg-oogvereniging-red px-5 py-3 mt-1 rounded-lg border border-oogvereniging-black
+                    shadow">Afmelden</button>
+                    </form>
+                @else
+                    <form action="{{ route('event.signup', $event->id) }}" method="POST">
+                        @csrf
+                        <button class="text-xl font-semibold tracking-wide text-oogvereniging-white
+                    bg-oogvereniging-red px-5 py-3 mt-1 rounded-lg border border-oogvereniging-black
+                    shadow">Direct aanmelden</button>
+                    </form>
+                @endif
             @endif
         </div>
         <div class="max-w-6xl mx-auto my-6">
