545bacfb09432bfbaa57a11938066f181bf43d16
Jelmer Rosdorf <jelmerr99@gmail.com>
2024-06-26 04:09:18 +0200

    UNDO Hosting test 1 - routing

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-//    return redirect('/login');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
