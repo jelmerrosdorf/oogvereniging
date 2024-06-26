7b4eb39b4af0440313fb39a0a1a75e3580d4796f
Jelmer Rosdorf <jelmerr99@gmail.com>
2024-06-26 04:06:06 +0200

    Hosting test 1 - routing

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/login');
+    return view('welcome');
+//    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
